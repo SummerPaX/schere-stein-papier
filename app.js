ff84012878a0872a121dd647e0eaeb54095704c0
Paulus Summer <paulus.summer@webundsoehne.com>
2023-01-20 09:01:20 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+"use strict";
+const emojis = ['✌', '👊', '✋'];
+const emojiPlayer = document.getElementById('emojiPlayer');
+const emojiComputer = document.getElementById('emojiComputer');
+const vsSpan = document.getElementById('vsSpan');
+const pointsPlayer = document.getElementById('pointsPlayer');
+const pointsComputer = document.getElementById('pointsComputer');
+const points = {
+    player: 0,
+    computer: 0
+};
+function attack(emoji) {
+    const randomNumber = Math.floor(Math.random() * 3);
+    const randomEmoji = emojis[randomNumber];
+    if (emojiPlayer != null)
+        emojiPlayer.textContent = emoji;
+    if (emojiComputer != null)
+        emojiComputer.textContent = randomEmoji;
+    if (emoji === randomEmoji) {
+        vsSpan === null || vsSpan === void 0 ? void 0 : vsSpan.style.setProperty('background', 'yellow');
+    }
+    else if ( // Win condition
+    (emoji === '✌' && randomEmoji === '✋') ||
+        (emoji === '👊' && randomEmoji === '✌') ||
+        (emoji === '✋' && randomEmoji === '👊')) {
+        vsSpan === null || vsSpan === void 0 ? void 0 : vsSpan.style.setProperty('background', 'green');
+        points.player++; // points.player = points.player + 1
+        if (pointsPlayer) {
+            pointsPlayer.textContent = points.player.toString();
+        }
+    }
+    else { // Lose condition
+        vsSpan === null || vsSpan === void 0 ? void 0 : vsSpan.style.setProperty('background', 'red');
+        points.computer++;
+        if (pointsComputer) {
+            pointsComputer.textContent = points.computer.toString();
+        }
+    }
+    vsSpan.q;
+}
